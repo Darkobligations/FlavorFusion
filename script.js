290ed625d9faad2c06bfa01bfbe972dd5c870d1b
Alijaha Nielsen <littlepaper10@gmail.com>
2023-06-09 20:40:55 -0400

    finished order menu

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+//Navigation Toggle Menu
 let menuOpen = document.querySelector('.navigation__wrapper');
 let navigation__toggle = document.querySelector('.navigation__toggle');
 
+//Navigation menu Open
 document.getElementById('menuToggle').addEventListener('click', function(){
     navigation__toggle.classList.toggle('active');
     menuOpen.classList.toggle('open');
+})
+
+
+//Order Toggle Menu
+let shoppingToggle = document.getElementById('shoppingToggle');
+let order_menu = document.querySelector('.navigation__order');
+let closeToggle = document.getElementById('closeToggle');
+
+shoppingToggle.addEventListener('click', function(){
+    order_menu.classList.toggle('open');
+})
+
+closeToggle.addEventListener('click', function(){
+    order_menu.classList.toggle('open');
 })
